8f5e7854c8b8b830bfced233338e82bdedd6bd38
manvendra2301 <manvendrapathya01@gmail.com>
2024-08-25 17:01:15 +0530

    final touchup

--- 06currencyConvertor/src/App.jsx
+++ 06currencyConvertor/src/App.jsx
@@ -5,10 +5,10 @@ import { InputBox } from "./components";
 import useCurrencyInfo from "./hooks/useCurrencyinfo";
 
 function App() {
-  const [amount, setAmount] = useState(0);
+  const [amount, setAmount] = useState("");
   const [from, setFrom] = useState("usd");
   const [to, setTo] = useState("inr");
-  const [convertedAmount, setConvertedAmount] = useState(0);
+  const [convertedAmount, setConvertedAmount] = useState("");
 
   const currencyInfo = useCurrencyInfo(from);
 
@@ -45,7 +45,7 @@ function App() {
                 label="From"
                 amount={amount}
                 currecyOptions={options}
-                onCurrencyChange={(currency) => setAmount(amount)}
+                onCurrencyChange={(currency) => setFrom(currency)}
                 selectCurrency={from}
                 onAmountChange={(amount) => setAmount(amount)}
               />
